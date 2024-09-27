381041d33e6608f757025b8bff34a6c2385b6313
RanjuRamya <142648272+RanjuRamya@users.noreply.github.com>
2024-09-27 23:14:15 +0530

    First commit

---
+++ Product_Management/src/main/java/com/example/demo/service/ProductImpl.java
@@ -0,0 +1,66 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Product;
+import com.example.demo.repository.ProductRepository;
+
+
+@Service
+public class ProductImpl implements ProductService {
+
+
+
+	@Autowired
+	private ProductRepository productRepo;
+	
+	@Override
+	public Product saveProduct(Product product) {
+	
+		return productRepo.save(product);
+	}
+
+	@Override
+	public List<Product>getAllProduct() {
+		
+		return productRepo.findAll();
+	}
+
+	@Override
+	public Product getProductById(Integer id) {
+		// TODO Auto-generated method stub
+		return productRepo.findById(id).get();
+	}
+
+	@Override
+	public String deleteProduct(Integer id) {
+		Product product=productRepo.findById(id).get();
+		
+		if(product!=null)
+		{
+			productRepo.delete(product);
+			return "Product Delete Sucessfull";
+		}
+		return "Something wrong on Server";
+		
+	}
+
+	@Override
+	public Product editProduct(Product p, Integer id) {
+
+			
+			
+			Product oldProduct=productRepo.findById(id).get();
+			oldProduct.setProductname(p.getProductname());
+			oldProduct.setDescription(p.getDescription());
+			oldProduct.setPrice(p.getPrice());
+			oldProduct.setStatus(p.getStatus());
+			
+			
+			return productRepo.save(oldProduct);
+		}
+	}
+
